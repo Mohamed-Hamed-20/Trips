bc8943f59dfb3ee041e5bf9f16f10bab4457bd33
Mohamed Hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2025-02-26 13:39:40 +0200

    fix auth error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,4 @@
-<<<<<<< Updated upstream
-import jwt from 'jsonwebtoken'
-import userModel from '../DB/model/user.model.js';
-import { asyncHandler } from '../services/asyncHandler.js';
-
-
-export const auth = () => {
-    return asyncHandler(async (req, res, next) => {
-        // try {
-        console.log({ bb: req.body });
-        const { authorization } = req.headers
-        const token = authorization.split(" ")[1]
-        
-        const decoded = jwt.verify(token, process.env.tokenSignature);
-        
-        if (!decoded?.id || !decoded?.isLoggedIn) {
-            // res.status(400).json({ message: "In-valid token payload " })
-            next(new Error("Invalid token payload ", { cause: 400 }))
-        } else {
-            const user = await userModel.findById(decoded.id).select('email userName role')
-            if (!user) {
-                // res.status(404).json({ message: "Not register user" })
-                next(new Error("Not register user ", { cause: 404 }))
-
-            } else {
-                req.user = user;
-                req.userId = user._id;
-                next()
-            }
-        }
-    })
-}
-=======
+// =======
 import jwt from "jsonwebtoken";
 import userModel from "../DB/model/user.model.js";
 import { asyncHandler } from "../services/asyncHandler.js";
@@ -45,9 +13,7 @@ export const auth = (
 ) => {
   return asyncHandler(async (req, res, next) => {
     // try {
-    console.log({ bb: req.body });
     const { authorization } = req.headers;
-    console.log({ authorization });
     if (!authorization?.startsWith(process.env.BearerKey)) {
       // res.status(400).json({ message: "In-valid Bearer key" })
       next(new Error("Invalid Bearer key", { cause: 400 }));
@@ -74,10 +40,5 @@ export const auth = (
         }
       }
     }
-    // } catch (error) {
-    //     res.status(500).json({ message: "catch error", error })
-
-    // }
   });
 };
->>>>>>> Stashed changes
