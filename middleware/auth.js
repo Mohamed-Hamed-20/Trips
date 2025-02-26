70b973514d06aa7b234791619550f9bba44d95d0
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-02-26 16:11:50 +0200

    auth update

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,11 +14,13 @@ export const auth = (
   return asyncHandler(async (req, res, next) => {
     // try {
     const { authorization } = req.headers;
-    if (!authorization?.startsWith(process.env.BearerKey)) {
+    if (!authorization?.startsWith("Bearer")) {
       // res.status(400).json({ message: "In-valid Bearer key" })
       next(new Error("Invalid Bearer key", { cause: 400 }));
     } else {
-      const token = authorization.split(process.env.BearerKey)[1];
+      const token = authorization.split(" ")[1];
+      console.log(token);
+      
       const decoded = jwt.verify(token, process.env.tokenSignature);
       if (!decoded?.id || !decoded?.isLoggedIn) {
         // res.status(400).json({ message: "In-valid token payload " })
@@ -33,6 +35,7 @@ export const auth = (
         } else {
           if (acceptRoles.includes(user.role)) {
             req.user = user;
+            req.userId = user.id;
             next();
           } else {
             next(new Error("Not authorized user ", { cause: 403 }));
